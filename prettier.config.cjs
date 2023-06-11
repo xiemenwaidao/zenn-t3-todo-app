25f2f275123f7e66c6a6793301721d11bfbb6722
xiemenwaidao <menxielijiao@gmail.com>
2023-06-12 01:02:40 +0900

    🚧 05.アプリケーションのセットアップをしよう(1)

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,8 @@
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+    plugins: [require.resolve("prettier-plugin-tailwindcss")],
+    printWidth: 160,
+    tabWidth: 4,
 };
 
 module.exports = config;
